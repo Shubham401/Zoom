7311a9f957010c357b95037b029a56839c074ad1
Shubham401 <shubham26.bdps@gmail.com>
2021-08-10 20:35:35 +0530

    added video functionality

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3030;
 const server = require('http').Server(app);
+const { v4: uuidv4 } = require('uuid');
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('room');
+    res.redirect(`/${uuidv4()}`);
+})
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room })
 })
 
 
